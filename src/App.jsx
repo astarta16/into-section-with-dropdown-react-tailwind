cd54625c9cd33ad82e4678131d3b445e64fdfe3c
astarta16 <ngedeshuri2001@gmail.com>
2023-08-18 22:39:59 +0400

    add basic styles and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import './App.css';
+import './index.css';
+import mobile from "./images/image-hero-mobile.png"
+import desktop from "./images/image-hero-desktop.png"
+
 
 function App() {
   return (
-<div>nini</div>
+<>
+<section className="grid grid-cols-1 gap-10 md:grid-cols-2 md:place-items-center max-w-4xl lg:max-w-4xl md:mx-auto">
+  <article>
+    <h1 className="font-bold text-4xl">Make remote work</h1>
+    <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+    <button>Learn more</button>
+  </article>
+
+  <article>
+    <picture>
+      <source media="(min-width:768px)" srcSet={desktop}/>
+      <img src={mobile} alt="main image" />
+    </picture>
+  </article>
+</section>
+</>
   );
 }
 
