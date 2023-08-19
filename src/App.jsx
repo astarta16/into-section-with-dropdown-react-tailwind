767cbe7d2307b86505dbe3e4f471a0a66ad25c75
astarta16 <ngedeshuri2001@gmail.com>
2023-08-19 10:13:50 +0400

    add button styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import desktop from "./images/image-hero-desktop.png"
 
 function App() {
   return (
+    
 <>
-<section className="grid grid-cols-1 gap-10 md:grid-cols-2 md:place-items-center max-w-4xl lg:max-w-4xl md:mx-auto">
+<section className="grid grid-cols-1 gap-10 md:grid-cols-2 md:place-items-center max-w-5xl lg:max-w-4xl md:mx-auto">
   <article>
-    <h1 className="font-bold text-4xl">Make remote work</h1>
+    <h1 className="font-bold text-4xl lg:text-6xl mb-5">Make remote work</h1>
     <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-    <button>Learn more</button>
+    <button className="bg-black shadow text-white rounded-lg font-bold hover:opacity-75 transition-all duration-150 pt-2 pb-2 px-6 cursor-pointer">Learn more</button>
   </article>
 
   <article>
