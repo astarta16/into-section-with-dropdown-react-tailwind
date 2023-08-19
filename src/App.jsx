4b99c56a91432593a26871445e9360372d38b92d
astarta16 <ngedeshuri2001@gmail.com>
2023-08-19 11:48:53 +0400

    finish main part

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import desktop from "./images/image-hero-desktop.png"
 
 function App() {
   return (
-    
+
 <>
-<section className="grid grid-cols-1 gap-10 md:grid-cols-2 md:place-items-center max-w-5xl lg:max-w-4xl md:mx-auto">
-  <article>
+<section className="flex  flex-col-reverse py-20 md:grid  md:gap-10 md:grid-cols-2 md:place-items-center max-w-4xl lg:max-w-5xl md:mx-auto">
+  <article className="text-center md:text-left px-5 xl:px-0 mt-10 md:mt-0">
     <h1 className="font-bold text-4xl lg:text-6xl mb-5">Make remote work</h1>
     <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
     <button className="bg-black shadow text-white rounded-lg font-bold hover:opacity-75 transition-all duration-150 pt-2 pb-2 px-6 cursor-pointer">Learn more</button>
@@ -17,7 +17,7 @@ function App() {
   <article>
     <picture>
       <source media="(min-width:768px)" srcSet={desktop}/>
-      <img src={mobile} alt="main image" />
+      <img src={mobile} alt="main image" className="w-full" />
     </picture>
   </article>
 </section>
