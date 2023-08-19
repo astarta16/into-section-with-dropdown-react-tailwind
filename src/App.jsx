3fe37c2d7b788fe89af6c068b76feaa072a62818
astarta16 <ngedeshuri2001@gmail.com>
2023-08-19 20:21:39 +0400

    finish project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './index.css';
 import mobile from "./images/image-hero-mobile.png"
 import desktop from "./images/image-hero-desktop.png"
 import audiophile from "./images/client-audiophile.svg"
@@ -7,116 +6,156 @@ import maker from "./images/client-maker.svg"
 import meet from "./images/client-meet.svg"
 import logo from "./images/logo.svg"
 import chevronDown from "./images/icon-arrow-down.svg"
+import chevronUp from "./images/icon-arrow-up.svg"
 import menuOpen from "./images/icon-menu.svg"
 import closeMenu from "./images/icon-close-menu.svg"
 import todolist from "./images/icon-todo.svg"
 import calendar from "./images/icon-calendar.svg"
 import reminders from "./images/icon-reminders.svg"
 import planning from "./images/icon-planning.svg"
+import { useState } from 'react';
 
 
 function App() {
+  const [openFeatures, setOpenFeatures] = useState(false)
+  const [openCompany, setOpenCompany] = useState(false)
+
   const handleOpenMenu = () => {
     const navbar = document.querySelector(".navbar")
     const menuButton = document.querySelector(".menu-button")
+
     navbar.classList.toggle("open")
 
-    if(navbar.classList.contains("open")) {
+    if (navbar.classList.contains("open")) {
       menuButton.src = closeMenu
     } else {
       menuButton.src = menuOpen
     }
   }
 
-
-
-
   return (
-<>
-<header className="p-5 flex items-center justify-between">
-  <div>
-    <img src={logo} alt="" />
-  <nav className="navbar">
-    <div>
-      <button> Features  <img src = {chevronDown} /></button>
-    </div>
+    <>
+      <header className="p-5 flex items-center justify-between">
+        <div className="lg:flex lg:items-start lg:justify-start">
+          <img src={logo} alt="" className="lg:mr-5" />
+          <nav className="navbar">
+            <div>
+              <button
+                onClick={() => setOpenFeatures(!openFeatures)}
+                className="flex items-center justify-start"
+              >
+                Features{" "}
+                {openFeatures ? (
+                  <img src={chevronUp} alt="" className="ml-2" />
+                ) : (
+                  <img src={chevronDown} alt="" className="ml-2" />
+                )}
+              </button>
+              {openFeatures && (
+                <ul className="mt-2 ml-3 lg:ml-0 lg:p-3 lg:shadow">
+                  <li className="flex items-center justify-start text-sm mb-2">
+                    <img src={todolist} alt="" className="mr-2" /> Todo List
+                  </li>
+                  <li className="flex items-center justify-start text-sm mb-2">
+                    <img src={calendar} alt="" className="mr-2" /> Calendar
+                  </li>
+                  <li className="flex items-center justify-start text-sm mb-2">
+                    <img src={reminders} alt="" className="mr-2" /> Reminders
+                  </li>
+                  <li className="flex items-center justify-start text-sm mb-2">
+                    <img src={planning} alt="" className="mr-2" /> Planning
+                  </li>
+                </ul>
+              )}
+            </div>
 
-    <ul className="mt-2">
-      <li className="flex items-center justify-start text-sm mb-2">
-        <img src={todolist} className="mr-2" /> Todo List
-      </li>
-      <li className="flex items-center justify-start text-sm mb-2">
-      <img src={calendar} className="mr-2" /> Calendar
-      </li>
-      <li className="flex items-center justify-start text-sm mb-2">
-      <img src={reminders} className="mr-2" /> Reminders
-      </li>
-      <li className="flex items-center justify-start text-sm mb-2">
-      <img src={planning}  className="mr-2" /> Planning
-      </li>
-    </ul>
+            <div>
+              <button
+                onClick={() => setOpenCompany(!openCompany)}
+                className="flex items-center justify-start"
+              >
+                Company{" "}
+                {openCompany ? (
+                  <img src={chevronUp} alt="" className="ml-2" />
+                ) : (
+                  <img src={chevronDown} alt="" className="ml-2" />
+                )}
+              </button>
+              {openCompany && (
+                <ul className="mt-2 ml-3 lg:ml-0 lg:p-3 lg:shadow">
+                  <li className="flex items-center justify-start text-sm mb-2">
+                    History
+                  </li>
+                  <li className="flex items-center justify-start text-sm mb-2">
+                    Our Team
+                  </li>
+                  <li className="flex items-center justify-start text-sm mb-2">
+                    Blog
+                  </li>
+                </ul>
+              )}
+            </div>
+            <div className="mb-2 lg:mb-0">
+              <button>Careers</button>
+            </div>
+            <div>
+              <button>About</button>
+            </div>
+          </nav>
+        </div>
 
-    <div>
-      <button>
-        Company <img src = {chevronDown} />
-        </button>
-        <ul className="mt-2">
-      <li className="flex items-center justify-start text-sm mb-2">History</li>
-      <li className="flex items-center justify-start text-sm mb-2">Our Team</li>
-      <li className="flex items-center justify-start text-sm mb-2">Blog</li>
-    </ul>
+        <div className="lg:hidden">
+          <button onClick={handleOpenMenu}>
+            <img src={menuOpen} alt="" className="menu-button" />
+          </button>
+        </div>
 
-    </div>
-    <div><button>Careers</button></div>
-    <div><button>About</button></div>
-  </nav>
-  </div>
+        <div className="hidden lg:block">
+          <button className="mr-10 opacity-75">Login</button>
+          <button className="border-2 border-black opacity-75 px-6 py-2 rounded-2xl">
+            Register
+          </button>
+        </div>
+      </header>
 
-  <div>
-    <button onClick={handleOpenMenu}>
-      <img  src={menuOpen} className="menu-button"/>
-    </button>
-  </div>
+      <section className="flex flex-col-reverse py-20 md:grid md:gap-10 md:grid-cols-2 md:place-items-center max-w-4xl lg:max-w-5xl md:mx-auto">
+        <article className="text-center md:text-left px-5 xl:px-0 mt-10 md:mt-0">
+          <h1 className="font-bold text-4xl lg:text-6xl xl:text-7xl mb-5">
+            Make remote work
+          </h1>
+          <p className="mb-5">
+            Get your team in sync, no matter your location. Streamline
+            processes, create team rituals, and watch productivity soar.
+          </p>
+          <button className="bg-black rounded-lg shadow text-white font-bold hover:opacity-75 transition-all duration-150 pt-3 pb-2 px-6 cursor-pointer">
+            Learn more
+          </button>
 
-<div className="hidden lg:block">
-  <button>Login</button>
-  <button>Sign up</button>
-</div>
-</header>
+          <ul className="grid grid-cols-4 gap-1 mt-10 place-items-center md:place-items-start">
+            <li>
+              <img src={databiz} alt="" className="w-16 md:w-24" />
+            </li>
+            <li>
+              <img src={audiophile} alt="" className="w-16 md:w-24" />
+            </li>
+            <li>
+              <img src={meet} alt="" className="w-16 md:w-24" />
+            </li>
+            <li>
+              <img src={maker} alt="" className="w-16 md:w-24" />
+            </li>
+          </ul>
+        </article>
 
-
-<section className="flex  flex-col-reverse py-20 md:grid  md:gap-10 md:grid-cols-2 md:place-items-center max-w-4xl lg:max-w-5xl md:mx-auto">
-  <article className="text-center md:text-left px-5 xl:px-0 mt-10 md:mt-0">
-    <h1 className="font-bold text-4xl lg:text-6xl mb-5">Make remote work</h1>
-    <p className="mb-5">Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-    <button className="bg-black shadow text-white rounded-lg font-bold hover:opacity-75 transition-all duration-150 pt-2 pb-2 px-6 cursor-pointer">Learn more</button>
-  
-  <ul className="grid grid-cols-4 gap-1 mt-10 place-items-center md:place-items-start">
-    <li> 
-      <img src={databiz} alt="" className="w-16 md:w-24" /> 
-      </li>
-    <li>
-      <img src={audiophile} alt="" className="w-16 md:w-24" /> 
-      </li>
-    <li>
-      <img src={meet} alt="" className="w-16 md:w-24" /> 
-      </li>
-    <li>
-      <img src={maker} alt="" className="w-16 md:w-24" /> 
-      </li>
-  </ul>
-  
-  </article>
-
-  <article>
-    <picture>
-      <source media="(min-width:768px)" srcSet={desktop}/>
-      <img src={mobile} alt="main image" className="w-full" />
-    </picture>
-  </article>
-</section>
-</>
-  );
+        <article>
+          <picture>
+            <source media="(min-width: 768px)" srcSet={desktop} />
+            <img src={mobile} alt="" className="w-full" />
+          </picture>
+        </article>
+      </section>
+    </>
+  )
 }
 
 export default App
